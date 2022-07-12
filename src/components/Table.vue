b458a754f41e912fae20f9fe10121c7d6d7bf402
Rafael <rafael.costa@bexpert.com>
2022-07-11 21:15:30 -0300

    colorful-type-tags

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -6,6 +6,32 @@
         <span class="id-text">#{{pokemon.id}}</span>
         <img v-bind:src="pokemon.sprites.front_default" alt="">
         <span class="name-text">{{pokemon.name}}</span>
+        <div class="types">
+          <span class="type"
+          v-for="(type, index) in pokemon.types"
+          v-bind:style="type.type.name=='normal' ? 'background:#A8A878' :
+          type.type.name=='fighting' ? 'background:#C03028' :
+          type.type.name=='flying' ? 'background:#A890F0' :
+          type.type.name=='poison' ? 'background:#A040A0' :
+          type.type.name=='ground' ? 'background:#E0C068' :
+          type.type.name=='rock' ? 'background:#B8A038' :
+          type.type.name=='bug' ? 'background:#A8B820' :
+          type.type.name=='ghost' ? 'background:#705898' :
+          type.type.name=='steel' ? 'background:#B8B8D0' :
+          type.type.name=='fire' ? 'background:#F08030' :
+          type.type.name=='water' ? 'background:#6890F0' :
+          type.type.name=='grass' ? 'background:#78C850' :
+          type.type.name=='electric' ? 'background:#F8D030' :
+          type.type.name=='psychic' ? 'background:#F85888' :
+          type.type.name=='ice' ? 'background:#98D8D8' :
+          type.type.name=='dragon' ? 'background:#7038F8' :
+          type.type.name=='dark' ? 'background:#68A090' :
+          type.type.name=='fairy' ? 'background:#EE99AC' :
+          type.type.name=='unknown' ? 'background:#68A090' : 
+          'background:grey'
+          " 
+          :key="index">{{type.type.name}}</span>
+        </div>
           <DetailsModal v-show="showModal" @close-modal="showModal=false"/>
 
       </div>
@@ -36,9 +62,10 @@
         // this.counter.length
       }).then(r =>{
         for (var p = 1; p <= 50; p++) {
-        api.get('https://pokeapi.co/api/v2/pokemon/' + p).then(response => {
-          this.pokemons.push(response.data) 
+        api.get('https://pokeapi.co/api/v2/pokemon/' +p+'').then(response => {
+          console.log(response.data)
           
+          this.pokemons.push(response.data);           
           localStorage.pokemonList = JSON.stringify(this.pokemons);   
         })
       }
@@ -51,6 +78,7 @@
     // filtro para a barra de pesquisa aceitar tanto nome quanto ID's
     computed: {
       filteredPokemons: function(){
+        console.log(+this.pokemons)
         return this.pokemons.filter((pokemon) =>{
           return pokemon.name.match(this.text) || JSON.stringify(pokemon.id).match(this.text);
         });
@@ -101,5 +129,20 @@
     position: absolute;
     right: 10px;
     top: 8px;
+  }
+
+  .types{
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+  }
+
+  .type{
+    text-transform: uppercase;
+    color: #fff;
+    padding: 2px 8px;
+    margin: 10px 0px;
+    border: 1px solid transparent;
+    border-radius: 4px;
   }
 </style>
